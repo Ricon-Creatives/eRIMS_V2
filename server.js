5ea9ac3f516ab72abea066ea0c53f276e79cc0f8
Isaac <isaactsibudarko@gmail.com>
2021-11-02 18:11:58 +0000

    prepped for heroku deployment v_8

--- server.js
+++ server.js
@@ -37,20 +37,18 @@ app.use('/api/user', user);
 app.use('/api/auth', auth);
 
 
-app.get("/", (req, res)=> res.status(200).send("Hello there"));
-
 
 //Serve static assets when in production
 if(process.env.NODE_ENV === 'production'){
     //Set static folder
-    app.use(express.static('client/build'));
+    app.use(express.static('./client/build'));
 
     app.get('*', (req,res) =>{
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     })
 }
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 
 
 app.listen (PORT, ()=> {
