f42908d077e0df8e0d9400b7b7441f708d77669f
Isaac <isaactsibudarko@gmail.com>
2021-11-02 16:52:21 +0000

    prepped for heroku deployment v_4

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
 const Sequelize = require('sequelize');
 const config = require('config');
 
-module.exports = new Sequelize( 'heroku_6606bba61418fe7', 'b9e263f531e879', 'c600b171', config.get('sqlConnect'));
+module.exports = new Sequelize( 'heroku_6606bba61418fe7', 'b9e263f531e879', 'c600b171',  config.get('sqlConnect'));
